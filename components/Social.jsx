9e001379ecdd32032a2cf64b4e975a7be689000a
Mark <159596917+Markusssssu@users.noreply.github.com>
2024-07-10 06:44:43 +0000

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,7 +3,6 @@ import {FaGitlab, FaTelegram, FaDiscord, FaDonate} from "react-icons/fa"
 
 const socials = [
     {icon: <FaGitlab/>, path: "https://gitlab.com/mark2911527/mark/-/learn_gitlab" },
-    {icon: <FaTelegram/>, path: ""},
     {icon: <FaDiscord/>, path: "https://discordapp.com/users/896039036517507152/ ", },
     {icon: <FaDonate/>, path: "https://www.donationalerts.com/r/mark_339", },
     
